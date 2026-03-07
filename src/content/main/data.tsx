81ed1e8ebf103bf5834236832ccecd924a7ee044
ianSama0217 <ian20000217@gmail.com>
2026-03-07 13:55:46 +0800

    main content

---
+++ src/content/main/data.tsx
@@ -0,0 +1,52 @@
+export const testAccountOptions = [
+  { label: "全部", value: "all" },
+  { label: "帳戶 A", value: "account-a" },
+  { label: "帳戶 B", value: "account-b" },
+];
+
+export const testCategoryOptions = [
+  { label: "全部", value: "all" },
+  { label: "飲食", value: "food" },
+  { label: "交通", value: "transportation" },
+];
+
+export const timeTypeOptions = [
+  { label: "日", value: "day" },
+  { label: "週", value: "week" },
+  { label: "月", value: "month" },
+  { label: "年", value: "year" },
+  { label: "自訂", value: "custom" },
+];
+
+export const testRecordData = [
+  {
+    category: "飲食",
+    amount: "100",
+    date: "2023-01-01",
+    note: "午餐費用",
+  },
+  {
+    category: "交通",
+    amount: "50",
+    date: "2023-01-02",
+    note: "捷運費用",
+  },
+  {
+    category: "交通",
+    amount: "25",
+    date: "2023-01-02",
+    note: "公車費用",
+  },
+  //   {
+  //     category: "飲食",
+  //     amount: "500",
+  //     date: "2023-01-05",
+  //     note: "精緻早午餐",
+  //   },
+  //   {
+  //     category: "交通",
+  //     amount: "1450",
+  //     date: "2023-01-09",
+  //     note: "高鐵票",
+  //   },
+];
--- src/content/main/index.tsx
+++ src/content/main/index.tsx
@@ -1,5 +1,61 @@
+import Button from "../../component/button";
+import Icon from "../../component/icon";
+import Record from "../../component/record";
+import Select from "../../component/select";
+import {
+  testAccountOptions,
+  testCategoryOptions,
+  testRecordData,
+  timeTypeOptions,
+} from "./data";
+
 function Main() {
-  return <>Main Page</>;
+  return (
+    <div className="w-100 d-flex flex-column justify-content-center mb-3">
+      <p className="fs-1 text-center">總覽</p>
+      <div className="d-flex justify-content-evenly mb-3">
+        <Select
+          label={"帳戶"}
+          placeholder={"請選擇帳戶"}
+          options={testAccountOptions}
+          style={"col-3 col-sm-4"}
+        />
+        <Select
+          label={"類別"}
+          placeholder={"請選擇類別"}
+          options={testCategoryOptions}
+          style={"col-3 col-sm-4"}
+        />
+      </div>
+      <div className="d-flex justify-content-around align-items-center mb-3">
+        <Icon style={"fs-1 bi-caret-left"} />
+        <Select
+          label={"日期區間"}
+          placeholder={"請選擇日期區間"}
+          options={timeTypeOptions}
+          style={"col-3 col-sm-4"}
+        />
+        <Icon style={"fs-1 bi-caret-right"} />
+      </div>
+      <div className="d-flex flex-column justify-content-center mb-3">
+        {/* Record List */}
+        {testRecordData.map((record, index) => {
+          return (
+            <Record
+              disabled={true}
+              type={"expense"}
+              onChange={(): void => {}}
+              key={index}
+              {...record}
+            />
+          );
+        })}
+      </div>
+      <div className="text-center">
+        <Button text={"新增"} style={"col-6"} />
+      </div>
+    </div>
+  );
 }
 
 export default Main;
